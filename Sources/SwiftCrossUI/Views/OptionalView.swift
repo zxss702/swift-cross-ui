bca1c4a97caa8835602747cbef3c75b98b9a868f
知阳 <zxs20060621@icloud.com>
2026-05-01 12:41:42 +0800

    feat(animation): add transition support to container views

--- Sources/SwiftCrossUI/Views/OptionalView.swift
+++ Sources/SwiftCrossUI/Views/OptionalView.swift
@@ -10,8 +10,7 @@ public struct OptionalView<V: View> {
     }
 }
 
-extension OptionalView: View {
-}
+extension OptionalView: View {}
 
 extension OptionalView: TypeSafeView {
     typealias Children = OptionalViewChildren<V>
@@ -20,14 +19,11 @@ extension OptionalView: TypeSafeView {
         backend: Backend,
         snapshots: [ViewGraphSnapshotter.NodeSnapshot]?,
         environment: EnvironmentValues
-    ) -> OptionalViewChildren<V> {
-        // TODO: This is a conservative implementation, perhaps there are some situations
-        //   where we could usefully use the snapshots even if there are too many.
-        let snapshot = snapshots?.count == 1 ? snapshots?.first : nil
-        return OptionalViewChildren(
+    ) -> Children {
+        Children(
             from: view,
             backend: backend,
-            snapshot: snapshot,
+            snapshot: snapshots?.count == 1 ? snapshots?.first : nil,
             environment: environment
         )
     }
@@ -36,91 +32,67 @@ extension OptionalView: TypeSafeView {
         _ children: OptionalViewChildren<V>,
         backend: Backend
     ) -> Backend.Widget {
-        return backend.createContainer()
+        let container = backend.createContainer()
+        for (index, child) in children.widgets.enumerated() {
+            backend.insert(child.into(), into: container, at: index)
+            backend.setPosition(ofChildAt: index, in: container, to: .zero)
+        }
+        return container
     }
 
     func computeLayout<Backend: BaseAppBackend>(
         _ widget: Backend.Widget,
-        children: OptionalViewChildren<V>,
+        children: Children,
         proposedSize: ProposedViewSize,
         environment: EnvironmentValues,
         backend: Backend
     ) -> ViewLayoutResult {
-        let hasToggled: Bool
-        let result: ViewLayoutResult
-        if let view {
-            if let node = children.node {
-                result = node.computeLayout(
-                    with: view,
-                    proposedSize: proposedSize,
-                    environment: environment
-                )
-                hasToggled = false
-            } else {
-                let node = AnyViewGraphNode(
-                    for: view,
-                    backend: backend,
-                    environment: environment
-                )
-                children.node = node
-                result = node.computeLayout(
-                    with: view,
-                    proposedSize: proposedSize,
-                    environment: environment
-                )
-                hasToggled = true
-            }
-        } else {
-            hasToggled = children.node != nil
-            children.node = nil
-            result = ViewLayoutResult.leafView(size: .zero)
-        }
-        children.hasToggled = children.hasToggled || hasToggled
-
-        return result
+        children.state.update(
+            content: dynamicContent,
+            proposedSize: proposedSize,
+            environment: environment,
+            backend: backend
+        )
     }
 
     func commit<Backend: BaseAppBackend>(
         _ widget: Backend.Widget,
-        children: OptionalViewChildren<V>,
+        children: Children,
         layout: ViewLayoutResult,
         environment: EnvironmentValues,
         backend: Backend
     ) {
-        if children.hasToggled {
-            backend.removeAllChildren(of: widget)
-            if let node = children.node {
-                backend.insert(node.widget.into(), into: widget, at: 0)
-                backend.setPosition(ofChildAt: 0, in: widget, to: .zero)
-            }
-            children.hasToggled = false
+        children.state.commit(
+            widget: widget,
+            layout: layout,
+            environment: environment,
+            backend: backend
+        )
+    }
+
+    @MainActor
+    fileprivate var dynamicContent: DynamicTransitionContent? {
+        guard let view else {
+            return nil
         }
-
-        _ = children.node?.commit()
-
-        backend.setSize(of: widget, to: layout.size.vector)
+        return DynamicTransitionContent(
+            identity: DynamicTransitionIdentity(type: V.self),
+            view: AnyView(view),
+            transition: _optionalTransitionTrait(of: view) ?? .opacity,
+            canTransition: true
+        )
     }
 }
 
-/// Stores a view graph node for the view's child if present. Tracks whether
-/// the child has toggled since last time the parent was updated or not.
 class OptionalViewChildren<V: View>: ViewGraphNodeChildren {
-    /// The view graph node for the view's child if present.
-    var node: AnyViewGraphNode<V>?
-    /// Whether the view has toggled since the last non-dryrun update. `true`
-    /// if the first view update hasn't occurred yet.
-    var hasToggled = true
+    var state: DynamicTransitionState
 
     var widgets: [AnyWidget] {
-        return [node?.widget].compactMap { $0 }
+        state.widgets
     }
 
     var erasedNodes: [ErasedViewGraphNode] {
-        if let node {
-            [ErasedViewGraphNode(wrapping: node)]
-        } else {
-            []
-        }
+        state.erasedNodes
     }
 
     /// Creates storage for an optional view's child if present (which can change at
@@ -131,13 +103,22 @@ class OptionalViewChildren<V: View>: ViewGraphNodeChildren {
         snapshot: ViewGraphSnapshotter.NodeSnapshot?,
         environment: EnvironmentValues
     ) {
-        if let view {
-            node = AnyViewGraphNode(
-                for: view,
-                backend: backend,
-                snapshot: snapshot,
-                environment: environment
-            )
+        state = DynamicTransitionState()
+        guard let view else {
+            return
         }
+        let content = DynamicTransitionContent(
+            identity: DynamicTransitionIdentity(type: V.self),
+            view: AnyView(view),
+            transition: _optionalTransitionTrait(of: view) ?? .opacity,
+            canTransition: true
+        )
+        state.activeContent = content
+        state.activeNode = ErasedViewGraphNode(
+            for: content.transition.applyTransition(content.view, .identity),
+            backend: backend,
+            snapshot: snapshot,
+            environment: environment
+        )
     }
 }
